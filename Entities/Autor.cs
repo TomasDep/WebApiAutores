b7dfdb6f3d27aeebebeb475af7ace29a930f06ed
TomasDep <tomas.valenzuelacb@gmail.com>
2023-08-03 20:57:36 -0400

    ADD Validations

--- Entities/Autor.cs
+++ Entities/Autor.cs
@@ -1,14 +1,29 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace WebAPIAutores.Entities
 {
-    public class Autor
+    public class Autor : IValidatableObject
     { 
       public int ID { get; set; }
+      [Required(ErrorMessage = "The name of author is required")]
       public string Nombre { get; set; }
+      [Range(18, 120)]
+      public int Edad { get; set; }
       public List<Libro> Libro { get; set; }
+
+      public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+      {
+        if (!string.IsNullOrEmpty(Nombre)) {
+          var firstLetter = Nombre[0].ToString();
+          
+          if (firstLetter != firstLetter.ToUpper())
+            yield return new ValidationResult("The first letter most be Uppercase", new String[] { nameof(Nombre) });
+        }
+      }
     }
 }
--- Entities/Libro.cs
+++ Entities/Libro.cs
@@ -1,8 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+using WebAPIAutores.Validators;
+
 namespace WebAPIAutores.Entities
 {
     public class Libro
     {
         public int ID { get; set; }
+        [Required]
+        [FirstLetterUppercase]
         public string Titulo { get; set; }
         public int AutorID { get; set; }
         public Autor Autor{ get; set; }
