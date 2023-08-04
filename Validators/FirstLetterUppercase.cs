39ab8e401f840a5630841f36dedb3552871cc1f7
TomasDep <tomas.valenzuelacb@gmail.com>
2023-08-03 20:58:19 -0400

    ADD Validator

---
+++ Validators/FirstLetterUppercase.cs
@@ -0,0 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace WebAPIAutores.Validators
+{
+    public class FirstLetterUppercase : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value == null || String.IsNullOrEmpty(value.ToString()))
+                return ValidationResult.Success;
+
+            var firstWord = value.ToString()[0].ToString();
+
+            if (firstWord != firstWord.ToUpper())
+                return new ValidationResult("The first char needs to be uppercase");
+
+            return ValidationResult.Success;
+        }
+    }
+}
